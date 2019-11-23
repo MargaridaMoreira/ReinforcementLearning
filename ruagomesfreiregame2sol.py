95d0c914192d57e1ee91f06b066de1a8b2a2930f
MargaridaMoreira <margaridamoreira121@tecnico.ulisboa.pt>
2019-11-23 17:19:10 +0000

    Commented steps of QLearning

--- ruagomesfreiregame2sol.py
+++ ruagomesfreiregame2sol.py
@@ -10,10 +10,17 @@ class LearningAgent:
         # nA maximum number of action per state
         def __init__(self,nS,nA):
 
-                # define this function
                 self.nS = nS
                 self.nA = nA
-                # define this function
+
+                self.alpha = 0.9 #Learning rate
+                self.gamma = 0.75 #Discount rate
+                self.epsilon = 0.2 #Exploration/Exploitation balance
+
+                #initialize matrix with number of states per number of actions per state 
+                #(all with value "none" to identify if it has been already tested or not)
+                #we will update the value to a number if it was already tested
+                
               
         
         # Select one action, used when learning  
@@ -26,8 +33,11 @@ class LearningAgent:
                 # define this function
                 # print("select one action to learn better")
 
+                #random.uniform(0, 1) = current epsilon
+                #if current epsilon < self.epsilon then selected a random undiscovered action (none)
+                #else selected action with best reward 
+
                 a = 0
-                # define this function
                 return a
 
         # Select one action, used when evaluating
@@ -38,18 +48,27 @@ class LearningAgent:
         # a - the index to the action in aa
         def selectactiontoexecute(self,st,aa):
                 # define this function
-                a = 0
                 # print("select one action to see if I learned")
+
+                #Go to matrix and find the action with the best reward to execute 
+
+                a = 0
                 return a
 
 
         # this function is called after every action
-        # st - original state
+        # ost - original state
         # nst - next state
         # a - the index to the action taken
         # r - reward obtained
         def learn(self,ost,nst,a,r):
                 # define this function
                 #print("learn something from this data")
+
+                #finds max of Q[y][b] (row of ost)
+                #
+
+                #updates reward of matrix 
+                #Q[ost][a] = Q[ost][a] + self.alpha*(r + self.gamma*max - Q[ost][a])
                 
                 return
