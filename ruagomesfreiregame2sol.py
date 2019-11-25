17d3bc4bd0841c71828af4134d3beb6089dcb175
MargaridaMoreira <margaridamoreira121@tecnico.ulisboa.pt>
2019-11-25 08:32:04 +0000

    SelectActionToLearn done

--- ruagomesfreiregame2sol.py
+++ ruagomesfreiregame2sol.py
@@ -1,5 +1,6 @@
 import random
 import numpy as np
+import math
 
 # LearningAgent to implement
 # no knowledge about the environment can be used
@@ -18,12 +19,14 @@ class LearningAgent:
                 self.gamma = 0.4 #Discount rate
                 self.epsilon = 0.2 #Exploration/Exploitation balance
 
+                self.possibleActions = 0
+
                 #initialize matrix with number of states per number of actions per state 
                 #(all with value zero to identify if it has been already tested or not)
                 #we will update the value to another if it was already tested
                 # self.Q = [nS] * [nA] -> initialize with zero
                 
-                self.Q = np.array([[ 0 for i in range(nA)] for j in range(nS)])
+                self.Q = np.array([[ 0.0 for i in range(nA)] for j in range(nS)])
                 
               
         
@@ -39,7 +42,9 @@ class LearningAgent:
 
                 #change epsilon with time
                 
-                a = -1
+                a = 0
+                value = self.Q[st][a]
+                self.possibleActions = len(aa)
                 
                 #random.uniform(0, 1) = current epsilon
                 #if current epsilon < self.epsilon then selected a random action from aa or random undiscovered from aa(if there is one)
@@ -47,16 +52,16 @@ class LearningAgent:
 
                 if random.uniform(0, 1) < self.epsilon:
                         randomAction = random.choice(aa) 
-                        a = 0
                         for i in aa:
                                 if randomAction == i:
                                         break
                                 a+=1
                 else:
                         for i in range(len(aa)):
-                                if a < self.Q[st][i]:
-                                        a = i                     
-
+                                if value < self.Q[st][i]:
+                                        value = self.Q[st][i]  
+                                        a = i  
+                                                         
                 return a
 
         # Select one action, used when evaluating
@@ -72,12 +77,12 @@ class LearningAgent:
                 #Go to matrix and find the action with the best reward to execute 
                 #max = np.max(Q[st, :])
                 #max in possible actions (aa)
-                
                 a = 0
-                for i in aa:
-                        if self.Q[st][i] > a:
-                                a = self.Q[st][i]
-                
+                value = self.Q[st][a]
+                for i in range(len(aa)):
+                        if value < self.Q[st][i]:
+                                value = self.Q[st][i]  
+                                a = i  
                 return a
 
 
@@ -87,12 +92,15 @@ class LearningAgent:
         # a - the index to the action taken
         # r - reward obtained
         def learn(self,ost,nst,a,r):
-                # define this function
                 #print("learn something from this data")
 
                 #finds max of Q[y][b] (row of ost)
-                max = np.max(self.Q[nst, :])
-                #updates quality of matrix 
+                max = -math.inf
+                for i in range(self.possibleActions):
+                        if self.Q[nst][i] > max:
+                                max = self.Q[nst][i]
 
-                self.Q[ost][a] = self.Q[ost][a] + self.alpha*(r + self.gamma*max - self.Q[ost][a])
+                #max = np.max(self.Q[nst, :])
+                #updates quality in Q 
+                self.Q[ost,a] = self.Q[ost,a] + self.alpha*(r + self.gamma*max - self.Q[ost,a])
 
